f9ed20231c66baee931616d8264e48a9bfdded98
Ian on MacBook Pro <imacd20@gmail.com>
2015-09-19 21:13:57 -0700

    Add user input to save_princess

--- save_princess.rb
+++ save_princess.rb
@@ -2,14 +2,20 @@
 require './lib/board'
 require './lib/grid'
 
-n = 3
-until (n == 21) do
-  reader, writer = IO.pipe
-  grid = Array.grid n
-  writer.puts n
-  writer.puts grid
-  board = Board.new reader
-  puts grid
-  puts board.display_path_to_princess
-  n += 2
+number = ARGV.shift
+times = number.to_i || 21
+if times.odd?
+  n = 3
+  until (n == times) do
+    reader, writer = IO.pipe
+    grid = Array.grid n
+    writer.puts n
+    writer.puts grid
+    board = Board.new reader
+    puts grid
+    puts board.display_path_to_princess
+    n += 2
+  end
+else
+  puts "Please enter an odd number"
 end

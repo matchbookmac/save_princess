1674f8873a1bd76f07034a2aedf984a3af9c5b3c
Ian on MacBook Pro <imacd20@gmail.com>
2015-09-19 20:23:02 -0700

    Print grid and solution to stdout from save_princess

--- save_princess.rb
+++ save_princess.rb
@@ -3,11 +3,13 @@ require './lib/board'
 require './lib/grid'
 
 n = 3
-until (n == 101) do
+until (n == 21) do
   reader, writer = IO.pipe
+  grid = Array.grid n
   writer.puts n
-  writer.puts Array.grid n
+  writer.puts grid
   board = Board.new reader
-  board.display_path_to_princess
+  puts grid
+  puts board.display_path_to_princess
   n += 2
 end
